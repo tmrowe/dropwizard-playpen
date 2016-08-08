bed04b454f928a5fe62c4d4714e8effcbff4c229
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-08 13:26:59 +0100

    Added a health check for the value of template.

---
+++ application/src/main/java/com/playpen/dropwzard/health/TemplateHealthCheck.java
@@ -0,0 +1,50 @@
+package com.playpen.dropwzard.health;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.playpen.dropwzard.configuration.HelloWorldConfiguration;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class provides health checks for the template field of the {@link HelloWorldConfiguration} class.
+ */
+public class TemplateHealthCheck extends HealthCheck
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(TemplateHealthCheck.class);
+
+    private final String template;
+
+    public TemplateHealthCheck(String template)
+    {
+        LOGGER.debug("Created TemplateHealthCheck with template: {}", template);
+        this.template = template;
+    }
+
+    /**
+     * An example health check that ensures the template contains 'TEST'.
+     * @return A {@link Result} object, indicating whether this check passed or failed.
+     */
+    @Override
+    protected Result check()
+    {
+        String saying = String.format(template, "TEST");
+        Result result;
+        if (saying.contains("TEST"))
+        {
+            result = Result.healthy();
+        }
+        else
+        {
+            result = Result.unhealthy("template doesn't include a name");
+        }
+        LOGGER.debug("Returning health check result: {}", result);
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
