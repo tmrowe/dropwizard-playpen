aa831a40a1fae52ffcab0061e14723f92bde3403
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-08 11:31:16 +0100

    Added some logging and Javadoc to the Application class.

--- application/src/main/java/com/playpen/dropwzard/HelloWorldApplication.java
+++ application/src/main/java/com/playpen/dropwzard/HelloWorldApplication.java
@@ -4,6 +4,8 @@ import com.playpen.dropwzard.configuration.HelloWorldConfiguration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Starting point for the application.
@@ -12,15 +14,31 @@ import io.dropwizard.setup.Environment;
  */
 public class HelloWorldApplication extends Application<HelloWorldConfiguration>
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldApplication.class);
+
+    private static final String PROJECT_NAME = "hello-world";
+
+    /**
+     * Entry point for this DropWizard application.
+     * @param args Arguments to pass into the application. For instance the configuration file to start with.
+     * @throws Exception Thrown if any Exception was thrown by the underlying calls made by the application.
+     */
     public static void main(String... args) throws Exception
     {
+        LOGGER.info("Running Hello World Application.");
+        LOGGER.debug("Application running with arguments = {}", args);
         new HelloWorldApplication().run(args);
     }
 
+    /**
+     * Returns the projects name.
+     * @return The name of the project.
+     */
     @Override
     public String getName()
     {
-        return "hello-world";
+        LOGGER.debug("Returning project name: {}", PROJECT_NAME);
+        return PROJECT_NAME;
     }
 
     @Override
